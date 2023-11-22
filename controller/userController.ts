3fdf3f1a28332020a4c6999d9f7b526b85840a4a
Rufai-Ahmed <abbeyrufai234@gmail.com>
2023-11-22 11:52:39 +0100

    controller updated

--- controller/userController.ts
+++ controller/userController.ts
@@ -24,7 +24,42 @@ export const viewUser = async (req: Request, res: Response) => {
     const user = await userModel.find();
 
     return res.status(201).json({
-      msg: "User created",
+      msg: "Viewing users",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      msg: "Error creating",
+    });
+  }
+};
+export const viewOneUser = async (req: Request, res: Response) => {
+  try {
+    const { userID } = req.body;
+
+    const user = await userModel.findById(userID);
+
+    return res.status(201).json({
+      msg: "Viewing users",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      msg: "Error creating",
+    });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const { userID } = req.body;
+
+    const user = await userModel.findById(userID);
+
+    return res.status(201).json({
+      msg: "Viewing users",
       data: user,
     });
   } catch (error) {
